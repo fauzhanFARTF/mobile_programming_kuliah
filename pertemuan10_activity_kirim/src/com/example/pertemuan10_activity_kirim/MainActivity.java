03caf0d44d83978f4a54ea5e67d22dd0bca29329
fauzhanFARTF <fauzan1812@gmail.com>
2023-01-26 16:51:50 +0700

    Final Latihan

---
+++ pertemuan10_activity_kirim/src/com/example/pertemuan10_activity_kirim/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.pertemuan10_activity_kirim;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends Activity {
+
+    //Initializing variables
+  	EditText inputName;
+  	EditText inputEmail;
+      @Override
+      protected void onCreate(Bundle savedInstanceState) {
+          super.onCreate(savedInstanceState);
+          setContentView(R.layout.activity_main);
+          
+          inputName = (EditText)findViewById(R.id.name);
+          inputEmail = (EditText)findViewById(R.id.email);
+          Button btnNextScreen = (Button)findViewById(R.id.btnNextScreen);
+          
+          //Listening to button event
+          btnNextScreen.setOnClickListener(new View.OnClickListener() {
+  			
+  			@Override
+  			public void onClick(View arg0) {
+  				// TODO Auto-generated method stub
+  				//Starting a new Intent
+  				Intent nextScreen = new Intent(getApplicationContext(),SecondActivity.class);
+  				//Sending data to another Activity
+  				nextScreen.putExtra("name", inputName.getText().toString());
+  				nextScreen.putExtra("email", inputEmail.getText().toString());
+  				
+  				//Starting new activity
+  				startActivity(nextScreen);
+  			}
+  		});
+      }
+  
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
